496950e44b3cebd5a9c6ed2adb614a558d8d725e
Arnab Saha <arnab.saha.id@gmail.com>
2020-05-25 10:57:01 -0500

    hello world pytest

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,8 @@
 class Point():
-    pass
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.latitude = lat
+        self.longitude = lon
+    
+    def get_lat_lon(self):
+        return (self.latitude, self.longitude)

0cc5070e25c75a1069e53df17e90fe97b3d8d24e
Arnab Saha <arnab.saha.id@gmail.com>
2020-05-25 10:57:33 -0500

    testing exceptions

--- scripts/chp2/video3/mapmaker_exceptions_start.py
+++ scripts/chp2/video3/mapmaker_exceptions_start.py
@@ -1,7 +1,12 @@
 
 class Point():
     def __init__(self, name, latitude, longitude):
+        if not isinstance(name, str):
+            raise TypeError("City name provided must be string type")
         self.name = name
+
+        if not ((-90 <= latitude <= 90) and (-180 <= longitude <= 180)):
+            raise ValueError("Invalid latitude, longitude combination")
         self.latitude = latitude
         self.longitude = longitude
 
--- tests/chp2/video3/test_exceptions_start.py
+++ tests/chp2/video3/test_exceptions_start.py
@@ -1,4 +1,5 @@
 from scripts.chp2.video3.mapmaker_exceptions_start import Point
+import pytest
 
 
 def test_make_one_point():
@@ -6,7 +7,11 @@ def test_make_one_point():
     assert p1.get_lat_long() == (14.7167, 17.4677)
 
 
-def test_invalid_point_generation():  # TO DO
-    # with pytest.raises(Exception) as exp:
-    #     raise(Exception)
-    pass
+def test_invalid_point_generation():
+    with pytest.raises(ValueError) as exp:
+        Point("Paris", 14.4534, -555.8906)
+        assert str(exp.value) == "Invalid latitude, longitude combination"
+
+    with pytest.raises(TypeError) as exp:
+        Point(123, 14.4534, -34.8906)
+    assert str(exp.value) == "City name provided must be string type"
